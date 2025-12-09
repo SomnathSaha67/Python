8e69b60076212c272347010e5513321f5739cebd
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2025-12-09 05:11:10 +0000

    file i/o practice

--- 07_file_io_practice/text_read_write/write_basic_text.py
+++ 07_file_io_practice/text_read_write/write_basic_text.py
@@ -1,4 +1,5 @@
 with open("notes.txt","w") as file:
-    file.write("Meeting at 10 AM\n")
-    file.write("Lunch with Sarah at 1 PM\n")
-    file.write("Project deadline on Friday\n")
+   number_of_lines=int(input("Enter the number of lines you want to write to the file: "))
+   for i in range(number_of_lines):
+       line=input(f"Enter line {i+1}: ")
+       file.write(line + "\n")
