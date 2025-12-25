0c2b0f48189c29b0744ddc652d3e885373ed4e84
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2025-12-25 06:02:58 +0000

    swap numbers

---
+++ basics/02_swap_numbers.py
@@ -0,0 +1,6 @@
+num1=float(input("Enter the first number: "))
+num2=float(input("Enter the second number: "))
+print(f"Before swapping: num1 = {num1}, num2 = {num2}")
+# Swapping the numbers
+num1, num2 = num2, num1
+print(f"After swapping: num1 = {num1}, num2 = {num2}")
