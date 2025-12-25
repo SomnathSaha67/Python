69c4115a052147999f9d68588705b7f227e3189c
Somnath Saha <somnath.saha.u2024@iem.edu.in>
2025-12-25 06:10:09 +0000

    rectangle area

--- basics/03_rectangle_area.py
+++ basics/03_rectangle_area.py
@@ -1,1 +1,4 @@
-length=float(input())
+length=float(input("Enter the length of the rectangle: "))
+breadth=float(input("Enter the breadth of the rectangle: "))
+area=round(length*breadth, 3)
+print(f"The area of the rectangle is: {area}cm²")
